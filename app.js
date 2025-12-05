68c3a3dbed8b702d2bba6f20ab0d93f21b128bfa
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2025-12-05 21:43:27 +0530

    Update server configuration to listen on all interfaces and add nodemon for development

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ class App {
         // DB connection and server listener
         db.getConnection().then(() => {
             console.log('Database connection successful on startup');
-            this.app.listen(this.PORT, async () => {
+            this.app.listen(this.PORT,"0.0.0.0", async () => {
                 console.log(`Server is running on port http://localhost:${this.PORT}`);
             })
         }).catch((error) => {
