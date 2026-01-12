404fd802630dde07bbd91e9b95a2bdb1ff0c4c3f
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2026-01-12 14:54:20 +0530

    feat: Implement addSport functionality and update Swagger documentation for admin routes

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { adminVerifyToken } from "../middleware/authUser.js";
-import { getAllSports } from "../controllers/sportController.js";
+import { addSport, getAllSports } from "../controllers/adminControllers.js";
 
 const adminRouter = express.Router();
 
 adminRouter.get('/getAllSports', adminVerifyToken, (req, res) => {
     // #swagger.tags = ['Admin']
     // #swagger.description = 'Get all sports'
-    getAllSports(req, res);
+    getAllSports(req, res);    
+});
+
+adminRouter.post('/addSport', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Add a new sport'
+    addSport(req, res);
 });
 
 export default adminRouter;
