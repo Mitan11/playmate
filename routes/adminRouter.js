7e7c36d517a698b083b33611aba3016505549d6a
Mitan tank <141761215+Mitan11@users.noreply.github.com>
2026-01-16 13:24:04 +0530

    feat: Add dashboard statistics and metrics endpoints for admin routes

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { adminVerifyToken } from "../middleware/authUser.js";
-import { addSport, adminLogin, deleteSport, getAllSports, updateSport } from "../controllers/adminControllers.js";
+import { addSport, adminLogin, deleteSport, getAllSports, getBookingMetrics, getBookingReport, getDashboardStats, getRecentActivities, getRevenueReport, getSportMetrics, getUserReport, updateSport } from "../controllers/adminControllers.js";
 
 const adminRouter = express.Router();
 
@@ -32,6 +32,48 @@ adminRouter.delete('/deleteSport/:sport_id', adminVerifyToken, (req, res) => {
     // #swagger.tags = ['Admin']
     // #swagger.description = 'Delete sport by sport ID'
     deleteSport(req, res);
+});
+
+adminRouter.get('/dashboard/stats', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get dashboard statistics'
+    getDashboardStats(req, res);
+});
+
+adminRouter.get('/dashboard/sport/metrics', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get sport metrics for dashboard'
+    getSportMetrics(req, res);
+});
+
+adminRouter.get('/dashboard/booking/metrics', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get booking metrics for dashboard'
+    getBookingMetrics(req, res);
+});
+
+adminRouter.get('/dashboard/recent/activities', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get recent activities for dashboard'
+    getRecentActivities(req, res);
+});
+
+adminRouter.get('/dashboard/booking/report', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get booking report for dashboard'
+    getBookingReport(req, res);
+});
+
+adminRouter.get('/dashboard/revenue/report', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get revenue report for dashboard'
+    getRevenueReport(req, res);
+});
+
+adminRouter.get('/dashboard/user/report', adminVerifyToken, (req, res) => {
+    // #swagger.tags = ['Admin']
+    // #swagger.description = 'Get user report for dashboard'
+    getUserReport(req, res);
 });
 
 export default adminRouter;
